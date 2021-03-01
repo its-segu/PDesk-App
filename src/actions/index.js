f5a66d51aff8e37373dbdd96ad96ad8eff345a19
its-segu <its-segu@github.com>
2021-03-01 12:20:09 -0700

    store/actions edit

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,14 +4,6 @@ export const confirmedAppointment = (store, appointment) => {
 
 };
 
-export const deleteFromMySet = (store, deletedJoke) => {
-  var index = store.state.confirmedAppointments.indexOf(deletedJoke);
-  if (index > -1) {
-    store.state.myJokes.splice(index, 1);
-  }
-  store.setState();
-};
-
 export const showDialog = (store, open) =>  {
   console.log(open)
   const openStatus = open;
